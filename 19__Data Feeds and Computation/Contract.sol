f65ee4c1194d4e633c511c7b114f6cbdc8e4b003
ChangWeiHong <changwh97@gmail.com>
2022-09-08 01:01:55 +0800

    started lesson 19 import chainlink abi created contract import solidity ver ^0.6.7

---
+++ 19__Data Feeds and Computation/Contract.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.6.7;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    //https://cryptozombies.io/en/lesson/19/chapter/3
+}
